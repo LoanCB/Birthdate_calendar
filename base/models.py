da7ea2052bc6edffdedb6373dbd1728edb033eff
Loan <loanbillonnet@gmail.com>
2023-03-02 20:13:18 +0100

    Create necessary model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField('Prénom', max_length=20)
+    last_name = models.CharField('Nom', max_length=30)
+    birthdate = models.DateField("Date d'anniveraire")
+
+    class Meta:
+        verbose_name = 'Personne'
+        verbose_name_plural = 'Personnes'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
