d50e0c868454b7b6f6193c55ec858b07ad64b7bd
Loan <loanbillonnet@gmail.com>
2023-03-02 22:13:56 +0100

    Add model to admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.models import Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birthdate')
