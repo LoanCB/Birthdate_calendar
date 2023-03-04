c495ce3189216189af5d9a797477781f79cbe8ac
Loan <loanbillonnet@gmail.com>
2023-03-04 10:37:11 +0100

    catch error on send_birthday_notif script + use user notif tag

--- base/management/commands/send_birthday_notif.py
+++ base/management/commands/send_birthday_notif.py
@@ -1,5 +1,4 @@
 import datetime
-import os
 
 import requests
 from django.core.management import BaseCommand
@@ -15,14 +14,19 @@ class Command(BaseCommand):
         total = 0
         for person in Person.objects.all():
             if person.birthdate.strftime('%d-%m') == today:
-                requests.post(
-                    f"https://ntfy.sh/{os.getenv('TOPIC_NAME')}",
-                    data=f"Aujourd'hui est l'anniversaire de {person.get_full_name()} !",
-                    headers={
-                        "Title": f"Anniversaire {person.get_full_name()}",
-                        "Tags": "tada"
-                    }
-                )
-                total += 1
-        self.stdout.write(self.style.SUCCESS(f'Recherche terminée, {total} anniversaire(s) trouvé(s)'))
+                try:
+                    request = requests.post(
+                        f"https://ntfy.sh/{person.user.notification_tag}",
+                        data=f"Aujourd'hui est l'anniversaire de {person.get_full_name()} !",
+                        headers={
+                            "Title": f"Anniversaire {person.get_full_name()}",
+                            "Tags": "tada"
+                        }
+                    )
+                    request.raise_for_status()
+                    total += 1
+                except Exception as e:
+                    self.stderr.write(f"Une erreur s'est produite lors de l'envoi de la notification : {e}")
+        if total > 0:
+            self.stdout.write(f'{total} anniversaire(s) trouvé(s)')
 
