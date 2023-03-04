dc0df03d5f442414a100e7cf4090e42e1b9e6a86
Loan <loanbillonnet@gmail.com>
2023-03-04 11:05:23 +0100

    Create README

--- birthdate_calendar/settings.py
+++ birthdate_calendar/settings.py
@@ -26,10 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ADMIN = ('Loan', 'contact@loan-cb.fr')
-
-VERSION = '0.1.0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -142,8 +138,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Login
-LOGIN_URL = "login/"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "login/"
