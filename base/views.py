a1ead4c080eb92c8755703a49f7537114e548eb5
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2023-09-28 00:41:20 +0200

    feat: add login and blank home page

--- base/views.py
+++ base/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from base.forms import LoginForm
+
+
+def login_user(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        if user:
+            login(request, user)
+            return redirect(home)
+
+        # TODO error message
+
+    return render(request, 'base/login.html', {"form": form})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Vous avez bien été déconnecté")  # TODO show messages on base_site.html
+    return redirect(login_user)
+
+
+@login_required
+def home(request):
+    return render(request, 'base/home.html')
